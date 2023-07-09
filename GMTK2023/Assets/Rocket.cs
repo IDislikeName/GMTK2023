f8c8c0c2f85015184918647f99d9d4e8b8a5193d
IDislikeName <yilaisun@yahoo.com>
2023-07-09 05:43:21 -0400

    roc

--- GMTK2023/Assets/Rocket.cs
+++ GMTK2023/Assets/Rocket.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class Rocket : HasProperties
 {
+    Rigidbody2D rb;
+    public int speed;
+    Animator anim;
+    public bool moving = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (moving)
+        {
+            rb.velocity = transform.up * speed;
+        }
     }
     public void Rotate()
     {
         transform.rotation = Quaternion.Euler(0, 0, transform.eulerAngles.z -90f);
+    }
+    public void Launch()
+    {
+        anim.SetBool("Moving",true);
+        moving = true;
+        StartCoroutine(SelfDestruct());
+        
+    }
+    IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(3f);
+        moving = false;
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
     }
 }
